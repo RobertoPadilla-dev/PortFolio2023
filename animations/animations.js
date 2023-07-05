4572c17862b220ccf88d9105e613c59d1d55ca5f
Roberto Padilla <84651684+RobertoPadilla-dev@users.noreply.github.com>
2023-07-04 22:14:31 -0600

    Add files via upload

---
+++ animations/animations.js
@@ -0,0 +1,182 @@
+const texts = ["Exploring the frontiers of computational science and front-end development...",
+    "Building the bridge between creativity."];
+const typedTextElement = document.getElementById("typewriter");
+
+function typeText(text) {
+    let currentCharIndex = 0;
+    const typingAnimation = setInterval(() => {
+        typedTextElement.textContent = text.slice(0, currentCharIndex);
+        currentCharIndex++;
+        if (currentCharIndex > text.length) {
+            clearInterval(typingAnimation);
+            setTimeout(() => {
+                typedTextElement.textContent = "";
+                nextWord();
+            }, 1000);
+        }
+    }, 100);
+}
+
+let currentTextIndex = 0;
+
+function nextWord () {
+    const indexWord = texts[currentTextIndex];
+    currentTextIndex = (currentTextIndex + 1) % texts.length;
+    typeText(indexWord);
+}
+
+const carousel = document.querySelector(".carousel");
+let isDrraging = false, startX, startScrollLeft;
+const dragStart = (e) => {
+    isDrraging = true;
+    carousel.classList.add("dragging");
+    startX = e.pageX;
+    startScrollLeft = carousel.scrollLeft;
+}
+const dragging = (e) => {
+    if(!isDrraging) return;
+    carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+}
+
+const dragStop = () => {
+    isDrraging = false;
+    carousel.classList.remove("dragging");
+}
+
+const firstCardWidth = carousel.querySelector(".card").offsetWidth;
+const arrowsBtns = document.querySelectorAll(".wrapper .arrow");
+arrowsBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        carousel.scrollLeft += btn.id === "leftt" ? - firstCardWidth : firstCardWidth;
+    })
+})
+
+
+const carouselChildrens = [...carousel.children];
+let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
+
+carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
+    carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
+});
+
+carouselChildrens.slice(0, cardPerView).forEach(card => {
+    carousel.insertAdjacentHTML("beforeend", card.outerHTML);
+});
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
+
+nextWord();
+particlesJS(
+    {
+        "particles": {
+            "number": {
+                "value": 60,
+                "density": {
+                    "enable": true,
+                    "value_area": 800
+                }
+            },
+            "color": {
+                "value": "#ffffff"
+            },
+            "shape": {
+                "type": "circle",
+                "stroke": {
+                    "width": 0,
+                    "color": "#000000"
+                },
+                "polygon": {
+                    "nb_sides": 5
+                },
+                "image": {
+                    "src": "img/github.svg",
+                    "width": 100,
+                    "height": 100
+                }
+            },
+            "opacity": {
+                "value": 0.5,
+                "random": false,
+                "anim": {
+                    "enable": false,
+                    "speed": 1,
+                    "opacity_min": 0.1,
+                    "sync": false
+                }
+            },
+            "size": {
+                "value": 3,
+                "random": true,
+                "anim": {
+                    "enable": true,
+                    "speed": 40,
+                    "size_min": 0.1,
+                    "sync": false
+                }
+            },
+            "line_linked": {
+                "enable": true,
+                "distance": 150,
+                "color": "#ffffff",
+                "opacity": 0.4,
+                "width": 1
+            },
+            "move": {
+                "enable": true,
+                "speed": 6,
+                "direction": "none",
+                "random": false,
+                "straight": false,
+                "out_mode": "out",
+                "bounce": false,
+                "attract": {
+                    "enable": false,
+                    "rotateX": 600,
+                    "rotateY": 1200
+                }
+            }
+        },
+        "interactivity": {
+            "detect_on": "canvas",
+            "events": {
+                "onhover": {
+                    "enable": true,
+                    "mode": "repulse"
+                },
+                "onclick": {
+                    "enable": true,
+                    "mode": "push"
+                },
+                "resize": true
+            },
+            "modes": {
+                "grab": {
+                    "distance": 400,
+                    "line_linked": {
+                        "opacity": 1
+                    }
+                },
+                "bubble": {
+                    "distance": 400,
+                    "size": 40,
+                    "duration": 2,
+                    "opacity": 8,
+                    "speed": 3
+                },
+                "repulse": {
+                    "distance": 200,
+                    "duration": 0.4
+                },
+                "push": {
+                    "particles_nb": 4
+                },
+                "remove": {
+                    "particles_nb": 2
+                }
+            }
+        },
+        "retina_detect": true
+    }
+);
---
+++ animations/transitions.js
@@ -0,0 +1,30 @@
+window.sr = ScrollReveal();
+sr.reveal('.header_container', {
+    duration: 1000,
+    origin: 'bottom',
+    distance: '-100px'
+});
+
+sr.reveal('.presentation_container', {
+    duration: 2000
+});
+
+sr.reveal('.about_me_container', {
+    opacity: 0
+});
+
+sr.reveal('.skills_hobbies_container', {
+    easing: 'ease-in'
+});
+
+sr.reveal('.education', {
+    easing: 'ease-in'
+});
+
+sr.reveal('.projects', {
+    opacity: 0
+});
+
+sr.reveal('.contact', {
+    duration: 3000,
+});
